27c6c8b65853c293b9f132ad1884036179e1f7ac
brady diamond <brady.diamond27@gmail.com>
2022-08-15 12:39:23 -0700

    add basic dependancies/ delete unused components

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import '../App.css';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Footer />  
+    </div>
+  );
+}
+
+export default App;
