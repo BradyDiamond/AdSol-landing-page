a6b3967a052e06a4a48ebdd2614fe5affd74f5f8
brady diamond <brady.diamond27@gmail.com>
2022-08-16 12:50:57 -0700

    add paralax bg refactor page design

--- src/components/App.js
+++ src/components/App.js
@@ -9,34 +9,58 @@ import mountains from "./../img/mountains.png";
 
 
 
+// function App() {
+//   return (
+//     <div className='app-wrapper'>
+//       <div className="header">
+//         <Header />
+//         </div>
+//         <div className='wrapper'>
+        
+//         <img className="background" src={mountains}></img>
+//           <img className="foreground" src={skyline}></img>
+//         </div>
+//       <div className="content">
+//         <div className='slides'>
+//           <div className='col-6'>
+//             {/* <SlideShow /> */}
+//           </div>
+//           <div className='col-6'>
+//             {/* <Info/> */}
+//           </div>
+//         </div>
+//         <div className='slides'>
+//           <div className='col-12'>
+//             {/* <Info/> */}
+//           </div>
+//         </div>
+        
+        
+//       </div>
+//       <Footer />
+//     </div>
+//   );
+// }
+
+
+// 231f20
+
 function App() {
   return (
     <div className='app-wrapper'>
-      <div className="header">
+      <div className="nav-header">
         <Header />
         </div>
         <div className='wrapper'>
-        
-        <img className="background" src={mountains}></img>
+        <header>
+          <img className="background" src={mountains}></img>
           <img className="foreground" src={skyline}></img>
-        </div>
-      <div className="content">
-        <div className='slides'>
-          <div className='col-6'>
-            {/* <SlideShow /> */}
-          </div>
-          <div className='col-6'>
-            {/* <Info/> */}
-          </div>
-        </div>
-        <div className='slides'>
-          <div className='col-12'>
-            {/* <Info/> */}
-          </div>
-        </div>
+        </header>
         
-        
-      </div>
+        <div className='info-body'>
+          <Info />
+        </div>
+        </div>
       <Footer />
     </div>
   );
--- src/components/Info.js
+++ src/components/Info.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Info() {
   return (
-    <div>
+    <div className='info-text'>
 Build your web presence with a website, social media and Google Ads, but also reach out the old fashioned way with postcards, business cards, brochures, flyers and more!
     </div>
   )
