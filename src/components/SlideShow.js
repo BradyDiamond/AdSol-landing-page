84d707046ebe7ee68e32fc3a8639d32524e3f690
brady diamond <brady.diamond27@gmail.com>
2022-08-16 15:41:44 -0700

    finalize slide component

--- src/components/SlideShow.js
+++ src/components/SlideShow.js
@@ -19,7 +19,7 @@ export default function SlideShow() {
     <MDBCarousel showIndicators showControls fade>
     <MDBCarouselInner>
       <MDBCarouselItem className='active'>
-        <MDBCarouselElement src='https://mdbootstrap.com/img/Photos/Slides/img%20(15).webp' alt='...' />
+        <MDBCarouselElement src='https://images.unsplash.com/photo-1604250165700-adffa4dd11a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80' alt='...' />
         <MDBCarouselCaption>
           <h5>First slide label</h5>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
@@ -27,7 +27,7 @@ export default function SlideShow() {
       </MDBCarouselItem>
 
       <MDBCarouselItem>
-        <MDBCarouselElement src='https://mdbootstrap.com/img/Photos/Slides/img%20(22).webp' alt='...' />
+        <MDBCarouselElement src='https://images.unsplash.com/photo-1495757450029-09dbedacbc36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80' alt='...' />
         <MDBCarouselCaption>
           <h5>Second slide label</h5>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -35,12 +35,22 @@ export default function SlideShow() {
       </MDBCarouselItem>
 
       <MDBCarouselItem>
-        <MDBCarouselElement src='https://mdbootstrap.com/img/Photos/Slides/img%20(23).webp' alt='...' />
+        <MDBCarouselElement src='https://images.unsplash.com/photo-1604250165934-78133c454c24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80' alt='...' />
         <MDBCarouselCaption>
           <h5>Third slide label</h5>
           <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
         </MDBCarouselCaption>
       </MDBCarouselItem>
+
+      <MDBCarouselItem>
+        <MDBCarouselElement src='https://images.unsplash.com/photo-1643447727844-1e2e31544237?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80' alt='...' />
+        <MDBCarouselCaption>
+          <h5>Fourth slide label</h5>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </MDBCarouselCaption>
+      </MDBCarouselItem>
+
+
     </MDBCarouselInner>
   </MDBCarousel>
   );
