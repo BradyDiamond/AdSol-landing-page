6a6ecda8285f0edd13258e82a1a80a27514b36ba
brady diamond <brady.diamond27@gmail.com>
2022-08-17 14:15:00 -0700

    add title animation and fix resize footer gap

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,16 @@ function Header() {
       <img  src={mountains}></img>
       <p className='product-header-text-under'></p>
       </div>
-      <p className='product-header-text'>test text</p>
+      <p className='product-header-text'>
+      <span className="--i:1">A</span>
+      <span className="--i:2">R</span>
+      <span className="--i:3">M</span>
+      <span className="--i:4">A</span>
+      <span className="--i:5">N</span>
+      <span className="--i:6">I</span>
+      {/* <span className="--i:7">g</span> */}
+      <span classNae="--i:8">.</span>
+        </p>
       <img className="foreground" src={skyline}></img>
     </header>
   )
