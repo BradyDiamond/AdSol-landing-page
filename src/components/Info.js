1ae9816394055f5333acc906e8661d586e7e0fdf
brady diamond <brady.diamond27@gmail.com>
2022-08-17 22:48:24 -0700

    add body styling

--- src/components/Info.js
+++ src/components/Info.js
@@ -5,14 +5,15 @@ function Info() {
   return (
     <>
      <p className='product-header-text'>
-      <span className="--i:1">A</span>
-      <span className="--i:2">R</span>
-      <span className="--i:3">M</span>
-      <span className="--i:4">A</span>
-      <span className="--i:5">N</span>
-      <span className="--i:6">I</span>
-      <span className="--i:7">g</span>
-      <span classNae="--i:8">.</span>
+      <span classNae="--i:8">-</span>
+      <span className="--i:1">S</span>
+      <span className="--i:2">A</span>
+      <span className="--i:3">T</span>
+      <span className="--i:4">I</span>
+      <span className="--i:5">R</span>
+      <span className="--i:6">e</span>
+      <span className="--i:7">-</span>
+      
       </p>
     <div className='info-text'>
       <img  src={bottle}></img>
